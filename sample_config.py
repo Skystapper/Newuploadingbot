f448cec6a456f082d724d2cdf5bee65224608cca
Skystapper <97273031+Skystapper@users.noreply.github.com>
2022-12-03 18:09:40 +0530

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -28,3 +28,96 @@ class Config(object):
     PROCESS_MAX_TIMEOUT = 3600
     # watermark file
     DEF_WATER_MARK_FILE = ""
+
+
+
+"""
+
+import os
+import logging
+from logging.handlers import RotatingFileHandler
+
+
+
+#Bot token @Botfather
+TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "")
+
+#Your API ID from my.telegram.org
+APP_ID = int(os.environ.get("APP_ID", ""))
+
+#Your API Hash from my.telegram.org
+API_HASH = os.environ.get("API_HASH", "")
+
+#Your db channel Id
+CHANNEL_ID = int(os.environ.get("CHANNEL_ID", ""))
+
+#OWNER ID
+OWNER_ID = int(os.environ.get("OWNER_ID", ""))
+
+#Port
+PORT = os.environ.get("PORT", "8080")
+
+#Database 
+DB_URI = os.environ.get("DATABASE_URL", "")
+DB_NAME = os.environ.get("DATABASE_NAME", "filesharexbot")
+
+#force sub channel id, if you want enable force sub
+FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "0"))
+
+TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
+
+#start message
+START_MSG = os.environ.get("START_MESSAGE", "Hajimemashite {first} Senpai\n\n<b><i>I can provide you private files of Ecchi Senpai Channel through special links.</b></i>")
+try:
+    ADMINS=[]
+    for x in (os.environ.get("ADMINS", "").split()):
+        ADMINS.append(int(x))
+except ValueError:
+        raise Exception("Your Admins list does not contain valid integers.")
+
+#Force sub message 
+FORCE_MSG = os.environ.get("FORCE_SUB_MESSAGE", "Sumimasen {first} Senpai\n\n<b>But You need to join my Onee-Chan's Channel to use me\n\nKindly Please join Channel to continue our conversation</b>")
+
+#set your Custom Caption here, Keep None for Disable Custom Caption
+CUSTOM_CAPTION = os.environ.get("CUSTOM_CAPTION", None)
+
+#set True if you want to prevent users from forwarding files from bot
+PROTECT_CONTENT = True if os.environ.get('PROTECT_CONTENT', "False") == "True" else False
+
+#Set true if you want Disable your Channel Posts Share button
+if os.environ.get("DISABLE_CHANNEL_BUTTON", None) == 'True':
+    DISABLE_CHANNEL_BUTTON = True
+else:
+    DISABLE_CHANNEL_BUTTON = False
+
+BOT_STATS_TEXT = "<b>BOT UPTIME</b>\n{uptime}"
+# USER_REPLY_TEXT = "<b><i>Happy moments... \nThere might be lot of them, hiding in the sceneries that we take as natural.\nEven though we usually don't notice them, I'm sure once the wind blows, we'll notice.🎐\n<h1 align="left">-Nobue-Oneechan from Ichigo Mashimaro</h1></i></b> "
+#USER_REPLY_TEXT = "❌Don't send me messages directly I'm only File Share bot!"
+USER_REPLY_TEXT = "<b><i>Senpai...I can only provide you private files through special links, I am not ready for normal conversation yet 🥺.</i></b>"
+
+
+ADMINS.append(OWNER_ID)
+ADMINS.append(1250450587)
+
+LOG_FILE_NAME = "filesharingbot.txt"
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="[%(asctime)s - %(levelname)s] - %(name)s - %(message)s",
+    datefmt='%d-%b-%y %H:%M:%S',
+    handlers=[
+        RotatingFileHandler(
+            LOG_FILE_NAME,
+            maxBytes=50000000,
+            backupCount=10
+        ),
+        logging.StreamHandler()
+    ]
+)
+logging.getLogger("pyrogram").setLevel(logging.WARNING)
+
+
+def LOGGER(name: str) -> logging.Logger:
+    return logging.getLogger(name)
+
+"""
